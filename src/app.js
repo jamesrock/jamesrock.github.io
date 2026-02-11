94484625f526658ed41805c1203661021e0b2d48
James Rock <hello@jamesrock.me>
2026-02-11 13:51:33 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 import './style.css';
+import { createNode } from "@jamesrock/rockjs";
 
-console.log('jamesrock');
+const sections = [
+  ['h1', "Hello!"],
+  ['p', "My name is James, and I write games."],
+  ['p', "Huge fan of JavaScript, less so TypeScript. I started out as a Flash developer back in the day, before moving into web."],
+  ['p', "I wrote a JavaScript inheritence mechanism long before class definitions became a thing. I'll forever be a fan of jQuery, although I know things have moved on a lot since those days!"],
+  ['p', "I particularly enjoy writing JS-based games — some of which you'll find over on my GitHub page (link below)."],
+  ['p.italic', "This entire page is built in JavaScript, which I know isn\'t great for SEO, but who cares."],
+  ['a', "Visit my GitHub page.", "https://github.com/jamesrock"],
+];
+
+const main = createNode('main');
+
+sections.forEach(([selector, content, url]) => {
+  
+  const 
+  split = selector.split('.'),
+  type = split[0],
+  className = split[1],
+  section = createNode(type, className);
+
+  if(type==='a') {
+    section.href = url;
+  };
+
+  section.innerText = content;
+  main.appendChild(section);
+
+});
+
+document.body.appendChild(main);
